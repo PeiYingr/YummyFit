4fc0e60684e90abdb013e95202f11276c66e66d8
PeiYing <peggy2000000@gmail.com>
2023-02-12 17:47:34 +0800

    feat model of meal photo

--- model/photo.js
+++ model/photo.js
@@ -18,6 +18,35 @@ const photoModel = {
         }finally{
             conn.release();
         }
+    },
+    addMealPhoto: async(userID, date, whichMeal, mealCloudFrontUrl) => {
+        const conn = await pool.getConnection();
+        try{
+            const data = [userID, date, whichMeal, mealCloudFrontUrl];
+            await conn.query("INSERT INTO mealPhoto(userID, date, meal, photo) VALUES (?, ?, ?, ?)", data);
+        }finally{
+            conn.release();
+        }
+    },
+    getMealPhoto: async(userID, date, meal) => {
+        const conn = await pool.getConnection();
+        try{
+            const data = [userID, date, meal];
+            const [result] = await conn.query("SELECT photo FROM mealPhoto WHERE userID = ? AND date = ? AND meal = ?", data);
+            return result
+        }finally{
+            conn.release();
+        }
+    },
+    deleteMealPhoto: async(userID, date, meal, photoUrl) => {
+        const conn = await pool.getConnection();
+        try{
+            const deletePhotoData = [userID, date, meal, photoUrl];
+            const sql = "DELETE FROM mealPhoto WHERE userID = ? AND date = ? AND meal = ? AND photo = ?";
+            await conn.query(sql, deletePhotoData);
+        }finally{
+            conn.release();
+        }
     }
 }
 
