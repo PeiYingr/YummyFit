487da67a52d05080206741e4a709b085b694d38c
PeiYing <peggy2000000@gmail.com>
2023-04-23 00:12:42 +0800

    modified indentation of code

--- router/user.js
+++ router/user.js
@@ -4,91 +4,43 @@ const userModel = require("../model/user")
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 require("dotenv").config({ path: ".env" });
-const JwtSecret=process.env.JWT_SECRET_KEY;
+const JwtSecret = process.env.JWT_SECRET_KEY;
 
 // signup
-userRouter.post("/", async(req, res) => {
-    try{
+userRouter.post("/", async (req, res) => {
+    try {
         const frontRequest = req.body;
         const name = frontRequest.name;
-		const email = frontRequest.email;
-		const password  = frontRequest.password;
-		if (name == "" || email == "" || password == ""){
-            res.status(400).json({ 			
+        const email = frontRequest.email;
+        const password = frontRequest.password;
+        if (name == "" || email == "" || password == "") {
+            res.status(400).json({
                 "error": true,
-                "message": "⚠️ Enter your name, Email and password" 
+                "message": "⚠️ Enter your name, Email and password"
             });
-        }else{
-			const result = await userModel.checkSignupEmail(email);
-			if (result){
-                res.status(400).json({ 			
+        } else {
+            const result = await userModel.checkSignupEmail(email);
+            if (result) {
+                res.status(400).json({
                     "error": true,
                     "message": "⚠️ Sorry! Email has been used!"
-                });               
-            }else{
-				const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-.]+){1,}$/;
+                });
+            } else {
+                const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-.]+){1,}$/;
                 const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/;
-				const emailResult= email.match(emailRegex);
-				const passwordResult = password.match(passwordRegex);
+                const emailResult = email.match(emailRegex);
+                const passwordResult = password.match(passwordRegex);
                 // 加強密碼安全性
                 const salt = await bcrypt.genSalt(10);
-			    const pwHash = await bcrypt.hash(password, salt);
-				if (emailResult && passwordResult){
-					await userModel.signup(name, email, pwHash);
+                const pwHash = await bcrypt.hash(password, salt);
+                if (emailResult && passwordResult) {
+                    await userModel.signup(name, email, pwHash);
                     // 直接讓使用者導向登入
                     const result = await userModel.signin(email);
                     const user = {
-                        "userID":result.userID,
-                        "name":result.name, 
-                        "email":result.email
-                    }
-                    const token = jwt.sign(user, JwtSecret);
-                    let response = {
-                        "ok": true
-                    }
-                    res.cookie("token", token, { httpOnly: true, expires: new Date(Date.now() + 3600000 * 24 * 7) });
-                    res.status(200).json(response);                   
-                }else if(emailResult == null){
-                    res.status(400).json({ 			
-                        "error": true,
-                        "message": "⚠️ Invalid Email"
-                    });                     
-                }else{
-                    res.status(400).json({ 			
-                        "error": true,
-                        "message": "⚠️ Password need to be 6-20 chars, at least one letter and one number!"
-                    }); 
-                } 
-            }
-        }		
-    }catch{
-        res.status(500).json({ 			
-            "error": true,
-            "message": "Server error" 
-        })
-    }
-})
-
-// signin
-userRouter.put("/", async(req, res) => {
-    try{
-        const frontRequest = req.body;
-        const email = frontRequest.email;
-        const password  = frontRequest.password;
-        if (email == "" || password == ""){
-            res.status(400).json({ 			
-                "error": true,
-                "message": "⚠️ Enter your Email and password" 
-            });
-        }else{
-            const result = await userModel.signin(email);
-            if (result){
-                const checkPassword = bcrypt.compareSync(password, result.password)
-                if (email == result.email && checkPassword){
-                    const user = {
-                        "userID":result.userID,
-                        "name":result.name, 
-                        "email":result.email
+                        "userID": result.userID,
+                        "name": result.name,
+                        "email": result.email
                     }
                     const token = jwt.sign(user, JwtSecret);
                     let response = {
@@ -96,33 +48,81 @@ userRouter.put("/", async(req, res) => {
                     }
                     res.cookie("token", token, { httpOnly: true, expires: new Date(Date.now() + 3600000 * 24 * 7) });
                     res.status(200).json(response);
-                }else{
-                    res.status(400).json({ 			
+                } else if (emailResult == null) {
+                    res.status(400).json({
+                        "error": true,
+                        "message": "⚠️ Invalid Email"
+                    });
+                } else {
+                    res.status(400).json({
+                        "error": true,
+                        "message": "⚠️ Password need to be 6-20 chars, at least one letter and one number!"
+                    });
+                }
+            }
+        }
+    } catch {
+        res.status(500).json({
+            "error": true,
+            "message": "Server error"
+        })
+    }
+})
+
+// signin
+userRouter.put("/", async (req, res) => {
+    try {
+        const frontRequest = req.body;
+        const email = frontRequest.email;
+        const password = frontRequest.password;
+        if (email == "" || password == "") {
+            res.status(400).json({
+                "error": true,
+                "message": "⚠️ Enter your Email and password"
+            });
+        } else {
+            const result = await userModel.signin(email);
+            if (result) {
+                const checkPassword = bcrypt.compareSync(password, result.password)
+                if (email == result.email && checkPassword) {
+                    const user = {
+                        "userID": result.userID,
+                        "name": result.name,
+                        "email": result.email
+                    }
+                    const token = jwt.sign(user, JwtSecret);
+                    let response = {
+                        "ok": true
+                    }
+                    res.cookie("token", token, { httpOnly: true, expires: new Date(Date.now() + 3600000 * 24 * 7) });
+                    res.status(200).json(response);
+                } else {
+                    res.status(400).json({
                         "error": true,
                         "message": "⚠️ Wrong Email or password"
-                    });   
-                }          
-            }else{
-                res.status(400).json({ 			
+                    });
+                }
+            } else {
+                res.status(400).json({
                     "error": true,
                     "message": "⚠️ Wrong Email or password"
-                }); 
+                });
             }
-        }        
-    }catch{
-        res.status(500).json({ 			
+        }
+    } catch {
+        res.status(500).json({
             "error": true,
-            "message": "Server error" 
+            "message": "Server error"
         })
     }
 })
 
 // get signin status/information
-userRouter.get("/", async(req, res) => {
-    try{
+userRouter.get("/", async (req, res) => {
+    try {
         const cookie = req.headers.cookie;
-        if (cookie){
-            const token = cookie.replace("token=","")
+        if (cookie) {
+            const token = cookie.replace("token=", "")
             const userCookie = jwt.verify(token, JwtSecret)
             let response = {
                 "data": {
@@ -132,25 +132,25 @@ userRouter.get("/", async(req, res) => {
                 }
             }
             res.status(200).json(response);
-        }else{
+        } else {
             res.status(403).json({
                 "error": true,
                 "message": "Access Denied.Please Login."
-            });        
-        }        
-    }catch{
-        res.status(500).json({ 			
+            });
+        }
+    } catch {
+        res.status(500).json({
             "error": true,
-            "message": "Server error" 
+            "message": "Server error"
         })
     }
 })
 
- // signout 
-userRouter.delete("/", async(req, res) => {
-	let response = {
-		"ok": true
-	}
+// signout 
+userRouter.delete("/", async (req, res) => {
+    let response = {
+        "ok": true
+    }
     res.clearCookie("token", "", { httpOnly: true, expires: new Date(0) });
     res.status(200).json(response);
 })
